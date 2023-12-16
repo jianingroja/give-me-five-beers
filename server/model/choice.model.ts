75f1397da152b6ded24fd09a90cda5e8bce77be7
wangjianing <jianingmica@gmail.com>
2023-12-16 13:57:28 +0100

    refactor: purify model's logic

--- server/model/choice.model.ts
+++ server/model/choice.model.ts
@@ -1,32 +1,28 @@
 import moment from 'moment';
-
 import { ChoiceModel, UserModel } from './index';
 
-type Choice = {
-  type: string;
-  userId: string;
-  choiceId: string;
-};
-
-const postBeerOption = async ({ type, userId, choiceId }: Choice) => {
-  const todayChoice = await ChoiceModel.findOne({
+const getTodayChoice = async (userId: string) => {
+  const res = ChoiceModel.findOne({
     user: userId,
     date: { $gte: moment().startOf('date'), $lte: moment().endOf('date') },
   });
 
-  if (todayChoice) {
-    // prevent multiple choice requests
-    return null;
-  }
-
-  const choiceRes = await ChoiceModel.create({ user: userId, type, choiceId });
-
-  await UserModel.findOneAndUpdate(
-    { _id: userId },
-    { $push: { choice: choiceRes.id } }
-  );
-
-  return choiceRes;
+  return res;
 };
 
-export { postBeerOption };
+const createChoice = async (userId: string, type: string, url: string) => {
+  const res = await ChoiceModel.create({ user: userId, type, url });
+
+  return res;
+};
+
+const updateUserChoice = async (userId: string, choiceId: string) => {
+  await UserModel.findOneAndUpdate(
+    { _id: userId },
+    { $push: { choice: choiceId } }
+  );
+
+  return true;
+};
+
+export { getTodayChoice, createChoice, updateUserChoice };
