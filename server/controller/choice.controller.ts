cce405ec1482459da4b22e7bb341581aa712d3df
wangjianing <jianingmica@gmail.com>
2023-12-17 13:12:22 +0100

    feat: get today choice

--- server/controller/choice.controller.ts
+++ server/controller/choice.controller.ts
@@ -6,11 +6,33 @@ import {
   createChoice,
   updateUserChoice,
 } from '../model/choice.model';
-import { BarResponseWithIndex, Brewery } from '../type/place';
+import { Bar, BarResponseWithIndex, Brewery } from '../type/place';
 
 const BASE_URL = {
   bar: process.env.BAR_BASE_URL,
   brewery: process.env.BREWERY_BASE_URL,
+};
+
+const getTodayBeerOption = async (req: Request, res: Response) => {
+  try {
+    const { userId } = req.params;
+
+    const todayChoice = await getTodayChoice(userId);
+
+    if (!todayChoice) {
+      throw "Can not find today's choice";
+    }
+
+    const { url, type } = todayChoice;
+    const result = await axios.get(url);
+    const info = result.data as Bar | Brewery;
+
+    res.status(200).send({ type, info });
+    return;
+  } catch (error) {
+    res.status(400).send(error);
+    return;
+  }
 };
 
 const postBeerOption = async (req: Request, res: Response) => {
@@ -24,32 +46,22 @@ const postBeerOption = async (req: Request, res: Response) => {
       throw 'not the right type of drink';
     }
 
-    // prevent multiple choices
-    const hasTodayChoice = await getTodayChoice(userId);
-    console.log(hasTodayChoice);
-    if (hasTodayChoice) {
-      throw 'only one drink per day';
-    }
-
-    // get random and send to client
+    // get random
+    let composedUrl = '';
     const url = BASE_URL[type];
     const randomUrl = `${url}/random`;
-    let composedUrl = '';
-
     const response = await axios.get(randomUrl);
-    if (type === 'bar') {
-      const randomBar = response.data as BarResponseWithIndex;
-      const { bar, index } = randomBar;
-      composedUrl = `${url}/${index}`;
 
-      res.status(201).send({ info: bar });
+    if (type === 'bar') {
+      // ? Is it necessary to get complete data here? Seems a bit of waste. Or just the index?
+      const randomBar = response.data as BarResponseWithIndex;
+      const { index } = randomBar;
+      composedUrl = `${url}/${index}`;
     }
     if (type === 'brewery') {
       const randomBrewery = (response.data as Brewery[])[0];
       const { id } = randomBrewery;
       composedUrl = `${url}/${id}`;
-
-      res.status(201).send({ info: randomBrewery });
     }
 
     // save random to database
@@ -57,10 +69,12 @@ const postBeerOption = async (req: Request, res: Response) => {
     const choiceId = choice.id;
     await updateUserChoice(userId, choiceId);
 
+    res.status(201).end();
     return;
   } catch (error) {
     res.status(400).send(error);
+    return;
   }
 };
 
-export { postBeerOption };
+export { getTodayBeerOption, postBeerOption };
