9391165bd2b6a92ca6c99deb0633a6ca03bde2bb
wangjianing <jianingmica@gmail.com>
2024-01-11 19:27:38 +0100

    refactor: remove bar

--- server/model/index.ts
+++ server/model/index.ts
@@ -1,15 +1,8 @@
 import { model } from 'mongoose';
-
-import {
-  todoSchema,
-  userSchema,
-  barSchema,
-  choiceSchema,
-} from '../schema/index';
+import { todoSchema, userSchema, choiceSchema } from '../schema/index';
 
 const TodoModel = model('todo', todoSchema);
 const UserModel = model('user', userSchema);
-const BarModel = model('bar', barSchema);
 const ChoiceModel = model('choice', choiceSchema);
 
-export { TodoModel, UserModel, BarModel, ChoiceModel };
+export { TodoModel, UserModel, ChoiceModel };
